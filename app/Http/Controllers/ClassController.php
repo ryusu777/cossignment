87eeb501a9c47a705311661c152b18280ad72634
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2021-12-09 13:45:08 +0700

    In classes, get classes as owner

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -11,8 +11,12 @@ class ClassController extends Controller
     public function index(Request $request) 
     {
         $user = $request->user();
+
+        // Classes as member
         $classes = $user->classes()
-            ->join('users', 'tr_classes.created_by', '=', 'users.id')
+            ->join('users', function ($join) {
+                $join->on('tr_classes.created_by', '=', 'users.id');
+            })
             ->select(
                 'tr_classes.class_id', 
                 'tr_classes.class_name', 
@@ -21,6 +25,21 @@ class ClassController extends Controller
                 'users.name as user_name'
             )
             ->get();
+        
+        // Classes as owner
+        $classes = $classes->merge($user->classesAsOwner()
+            ->join('users', function ($join) {
+                $join->on('tr_classes.created_by', '=', 'users.id');
+            })
+            ->select(
+                'tr_classes.class_id', 
+                'tr_classes.class_name', 
+                'tr_classes.class_description', 
+                'tr_classes.class_code',
+                'users.name as user_name'
+            )
+            ->get());
+
         return Inertia::render('Classes/ClassList', [
             'classes' => $classes,
         ]);
